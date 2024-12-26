e26b3bfaf76839034986d42208309e307e8bae19
YinCheng <eason010630@gmail.com>
2024-12-26 21:07:14 +0800

    fix: css, Add: Logo

--- src/components/WebTitle.vue
+++ src/components/WebTitle.vue
@@ -8,7 +8,8 @@ defineProps({
 </script>
 
 <template>
-  <div>
-    <h1 class="py-4 text-bold text-center text-6xl dark:text-white mix-md:text-3xl">{{ msg }}</h1>
+  <div class="flex justify-center mr-6">
+    <img class="p-2 mx-2 rounded-xl md:rounded-3xl w-20 h-20 md:w-24 md:h-24" src="../assets/logo.png" alt="TaiChung_Torulogo" />
+    <h1 class="py-4 text-bold text-center text-4xl md:text-6xl  dark:text-white mix-md:text-3xl">{{ msg }}</h1>
   </div>
 </template>
